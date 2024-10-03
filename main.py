d27cdca5a5be6f466eacc7fcd9c3341e2d9a214b
imtekbistro <imtekbistro@gmail.com>
2024-10-02 20:01:08 -0400

    Report code

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ def main():
     text = get_book_text(book_path)
     words = wordCount(text)
     character_counts = charCount(text)
-
-
-    print(text)
+    charsSortedList = charCountToSortedList(character_counts)
+    
+    print(f"--- Begin report of {book_path} ---")
     print(f"{words} words found in the document")
-    print(character_counts)
+    print()
+
+    for y in charsSortedList:
+        if not y["char"].isalpha():
+            continue
+        print(f"The '{y['char']}' character was found {y['num']} times")
+
+    print("--- End report ---")
+
+
+       
 
 
 def get_book_text(path):
@@ -27,5 +37,15 @@ def charCount(text):
         else:
             chars[letter] += 1
     return chars
+
+def charCountToSortedList(charCountDict):
+    sortedList =[]
+    for x in charCountDict:
+        sortedList.append({"char":x, "num":charCountDict[x]})
+    sortedList.sort(reverse=True, key=sortChars)
+    return(sortedList)
+
+def sortChars(dict):
+    return dict["num"]
 
 main()
